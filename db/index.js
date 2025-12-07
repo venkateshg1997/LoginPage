77352972f97445d31273d608ac72b3728e7746f7
Gnanaprakash Venkatesh <gnanaprakashvenkatesh@gmail.com>
2025-12-07 21:50:01 +0530

    eleven

--- db/index.js
+++ db/index.js
@@ -49,6 +49,7 @@ app.post('/login', function(req,res){
 })
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+
+app.listen(PORT, "0.0.0.0", () => {
   console.log("Server running on port " + PORT);
 });
